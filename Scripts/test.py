a286e50b7ff55358885c61860433e774dd04dcc1
SvenMokveldje <39305538+SvenMokveldje@users.noreply.github.com>
2021-04-28 13:19:46 +0200

    Added ping pong
    Added a ping and pong request via the added form on the html indexpage

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/")  # Here the main page is defined
 def home():
-    return "Test"
+    return render_template("Indexpage.html")
 
-if __name__==("__main__"):
+@app.route("/<name>")               # This allows the user to go to different pages instead of the homepage
+def user(name):                     # essentially it takes .../x the x as input and loads file x (for example: 127.0.0.1:5000/Indexpage.html loads Indexpage.html)
+    return render_template({name})
+
+# @app.route("/admin")  # This can be used to redirect user (for example now it redirects url/admin to url.)
+# def admin():          # not very relevant for us yet.
+#     return redirect(url_for("home"))
+
+@app.route("/", methods=['GET', 'POST'])    # Check for current page if there are HTML forms with the methods Get and Post
+def testbutton():                           #Function you want to define in the HTML form
+    if request.method == "POST":            #Check if form method was post
+        return "PONG"                       #Type pong on site
+    else:
+        return "Did not work"
+
+if __name__ == "__main__":
     app.run()
